5d46f932a25f73e3a53ac49f53732d64b15ee533
Abdul Moiz Siraj <abdulmoiz68@gmail.com>
2020-11-12 20:23:56 +0500

    added cards

---
+++ frontend/src/components/product.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const Product = ({ product }) => {
+  return (
+    <>
+      <Card className='my-2 p-3 rounded'>
+        <a href={`/product/${product._id}`}>
+          <Card.Img src={product.image} variant='top' />
+        </a>
+        <Card.Body>
+          <a href={`/product/${product._id}`}>
+            <Card.Title as='div' className='text-center'>
+              <strong>{product.name}</strong>
+            </Card.Title>
+          </a>
+
+          <Card.Text as='div' className='text-center'>
+            <div className='my-2'>
+              {product.rating} from {product.numReviews} reviews
+            </div>
+          </Card.Text>
+
+          <Card.Text as='h3' className='text-center'>
+            ${product.price}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default Product;
