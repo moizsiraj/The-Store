1071ef4476c136ff1b1f7a49d9c9815d92044b0d
Abdul Moiz Siraj <abdulmoiz68@gmail.com>
2021-06-15 09:58:08 +0500

    redux integrated

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productsRoutes.js';
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const app = express();
 
 dotenv.config();
@@ -10,6 +10,10 @@ dotenv.config();
 connectDB();
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
   res.send('API running');
--- frontend/src/screens/homeScreen.js
+++ frontend/src/screens/homeScreen.js
@@ -1,28 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/product';
-import axios from 'axios';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { listProduct } from '../actions/productActions';
+import Loader from '../components/loader';
+import Message from '../components/message';
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  console.log(productList);
 
   useEffect(() => {
-    const getProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-    getProducts();
-  }, []);
+    dispatch(listProduct());
+  }, [dispatch]);
   return (
     <>
       <h1 className='text-center'>Check Out Our Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
